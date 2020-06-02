5af229ba69527daf3662b1899ed985fa9404251b
Jay McDoniel <jmcdo29@gmail.com>
2020-06-02 09:22:55 -0700

    fix: adds back AppModule to allow for running server for tests

--- test/app/main.ts
+++ test/app/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(
-    AppModule.forRoot(),
+    AppModule,
     // new ExpressAdapter(),
     new FastifyAdapter(),
   );
