3a0cf2ed70c7abbe02e9d96f26ab2c81b3c7bb2f
Kim 金可明 <koomen@protonmail.com>
2020-06-03 23:29:40 +0800

    feat: Implement basic support for websocket

--- src/throttler.exception.ts
+++ src/throttler.exception.ts
@@ -1,7 +1,14 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
+import { WsException } from '@nestjs/websockets';
 
 export class ThrottlerException extends HttpException {
   constructor() {
     super('ThrottlerException: Too Many Requests', HttpStatus.TOO_MANY_REQUESTS);
+  }
+}
+
+export class ThrottlerWsException extends WsException {
+  constructor() {
+    super('ThrottlerWsException: Too Many Requests');
   }
 }
--- test/app/gateways/app.gateway.ts
+++ test/app/gateways/app.gateway.ts
@@ -1,23 +1,28 @@
-import { WebSocketGateway, SubscribeMessage } from '@nestjs/websockets';
+import { UseGuards } from '@nestjs/common';
+import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { SkipThrottle, Throttle, ThrottlerGuard } from '../../../src';
 import { AppService } from '../app.service';
-import { SkipThrottle, Throttle } from '../../../src';
 
 @Throttle(2, 10)
 @WebSocketGateway({ path: '/' })
 export class AppGateway {
   constructor(private readonly appService: AppService) {}
+
+  @UseGuards(ThrottlerGuard)
   @SubscribeMessage('throttle-regular')
   pass() {
     return this.appService.success();
   }
 
   @SkipThrottle()
+  @UseGuards(ThrottlerGuard)
   @SubscribeMessage('ignore')
   ignore() {
     return this.appService.ignored();
   }
 
   @Throttle(5, 20)
+  @UseGuards(ThrottlerGuard)
   @SubscribeMessage('throttle-override')
   throttleOverride() {
     return this.appService.success();
