002d583c31b61fec3bfdc20ba72e5be751c36875
Kim 金可明 <koomen@protonmail.com>
2020-06-06 17:15:08 +0800

    feat: Make storage methods async

--- src/throttler-storage.interface.ts
+++ src/throttler-storage.interface.ts
@@ -10,13 +10,13 @@ export interface ThrottlerStorage {
   /**
    * Get a record via its key and return all its request ttls.
    */
-  getRecord(key: string): number[] | undefined;
+  getRecord(key: string): Promise<number[]>;
 
   /**
    * Add a record to the storage. The record will automatically be removed from
    * the storage once its TTL has been reached.
    */
-  addRecord(key: string, ttl: number): void;
+  addRecord(key: string, ttl: number): Promise<void>;
 }
 
 export const ThrottlerStorage = Symbol('ThrottlerStorage');
