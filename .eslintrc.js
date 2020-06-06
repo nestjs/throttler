ff8d82dc2c1bc6d7f242d0895e908d4544ee4759
Jay McDoniel <jmcdo29@gmail.com>
2020-06-06 16:17:38 -0700

    chore(tooling): adds tooling for conventional-commit
    Adds tools for conventional-commit lint-staged, git-cz and others to help keep in line the
    methodologies for committing and documentation.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
@@ -21,15 +20,18 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'max-len': ['warn', {
-      code: 100,
-      tabWidth: 2,
-      ignoreComments: false,
-      ignoreTrailingComments: true,
-      ignoreUrls: true,
-      ignoreStrings: true,
-      ignoreTemplateLiterals: true,
-      ignoreRegExpLiterals: true
-    }]
+    'max-len': [
+      'warn',
+      {
+        code: 100,
+        tabWidth: 2,
+        ignoreComments: false,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
   },
 };
