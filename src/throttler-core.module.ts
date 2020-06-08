833b616dededf435255b4403b266eb28d5df05a0
Kim 金可明 <koomen@protonmail.com>
2020-06-08 19:08:08 +0800

    feat: remove global guard and require user to implement it manually

--- src/throttler-core.module.ts
+++ src/throttler-core.module.ts
@@ -1,11 +1,9 @@
 import { createConfigurableDynamicRootModule } from '@golevelup/nestjs-modules';
 import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerStorage } from './throttler-storage.interface';
 import { THROTTLER_OPTIONS } from './throttler.constants';
-import { ThrottlerGuard } from './throttler.guard';
 import { ThrottlerOptions } from './throttler.interface';
 import { ThrottlerStorageService } from './throttler.service';
-import { ThrottlerStorage } from './throttler-storage.interface';
 
 @Module({})
 export class ThrottlerCoreModule extends createConfigurableDynamicRootModule<
@@ -19,10 +17,6 @@ export class ThrottlerCoreModule extends createConfigurableDynamicRootModule<
       useFactory: (options: ThrottlerOptions) => {
         return options.storage ? options.storage : new ThrottlerStorageService();
       },
-    },
-    {
-      provide: APP_GUARD,
-      useClass: ThrottlerGuard,
     },
   ],
 }) {}
