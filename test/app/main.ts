ef1437391cd11bf9e118b0d9e38fcd59d8bd6f1a
Kim 金可明 <koomen@protonmail.com>
2020-06-09 22:49:11 +0800

    test: setup main.ts and app.module.ts to their correct state

--- test/app/main.ts
+++ test/app/main.ts
@@ -1,16 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-// import { FastifyAdapter } from '@nestjs/platform-fastify';
-import { ExpressAdapter } from '@nestjs/platform-express';
-// import { WsAdapter } from '@nestjs/platform-ws';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
+import { WsAdapter } from '@nestjs/platform-ws';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(
     AppModule,
-    new ExpressAdapter(),
-    // new FastifyAdapter(),
+    // new ExpressAdapter(),
+    new FastifyAdapter(),
   );
-  // app.useWebSocketAdapter(new WsAdapter(app))
+  app.useWebSocketAdapter(new WsAdapter(app));
   await app.listen(3000);
 }
 bootstrap();
