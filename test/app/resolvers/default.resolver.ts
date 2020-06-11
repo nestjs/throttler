9bc26d08a3e63d90a3136f01abee3ad98506c40a
Jay McDoniel <jmcdo29@gmail.com>
2020-06-11 11:37:04 -0700

    feat(graphql): implements graphql limiter
    The GraphQL throttler works well for Express but it does have problems when it comes to fastify due
    to [this pull request](https://github.com/apollographql/apollo-server/pull/3895). Once this is
    merged in we can start looking at supporting Fastify as well for GraphQL.

--- test/app/resolvers/default.resolver.ts
+++ test/app/resolvers/default.resolver.ts
@@ -1,5 +1,18 @@
-import { Resolver } from '@nestjs/graphql';
+import { Mutation, Query, Resolver } from '@nestjs/graphql';
+import { AppService } from '../app.service';
 import { ResolveType } from './resolve.model';
 
 @Resolver(ResolveType)
-export class DefaultResolver {}
+export class DefaultResolver {
+  constructor(private readonly appService: AppService) {}
+
+  @Query(() => ResolveType)
+  defaultQuery() {
+    return this.appService.success();
+  }
+
+  @Mutation(() => ResolveType)
+  defaultMutation() {
+    return this.appService.success();
+  }
+}
