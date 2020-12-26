c44db969c5ff4fdeefb80753da324c38a000fb01
Andrew Dassonville <dassonville.andrew@gmail.com>
2020-12-26 01:16:10 -0800

    fix: properly export websocket exceptions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './throttler-module-options.interface';
 export * from './throttler-storage.interface';
+export * from './throttler-ws.exception';
 export * from './throttler.decorator';
 export * from './throttler.exception';
 export * from './throttler.guard';
