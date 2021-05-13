77fa1068df4a9aeaa1317aa6d91ad112223a3e07
Kim 金可明 <koomen@protonmail.com>
2021-05-13 17:50:25 +0800

    fix(guard): check if req.headers exists

--- src/throttler.guard.ts
+++ src/throttler.guard.ts
@@ -67,7 +67,7 @@ export class ThrottlerGuard implements CanActivate {
     // Return early if the current user agent should be ignored.
     if (Array.isArray(this.options.ignoreUserAgents)) {
       for (const pattern of this.options.ignoreUserAgents) {
-        if (pattern.test(req.headers['user-agent'])) {
+        if (req.headers && pattern.test(req.headers['user-agent'])) {
           return true;
         }
       }
