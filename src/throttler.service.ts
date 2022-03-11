e93036b3a6363ad10a53bd9d3f3892da959cc832
Achim Nohl <achim.nohl@online.de>
2022-03-11 17:10:13 +0100

    Fix: Memory leak in in service timeoutIds array
    Timeout Ids are not deleted and array is growing cont. causing heap memory issues.

--- src/throttler.service.ts
+++ src/throttler.service.ts
@@ -25,6 +25,7 @@ export class ThrottlerStorageService implements ThrottlerStorage, OnApplicationS
     const timeoutId = setTimeout(() => {
       this.storage[key].shift();
       clearTimeout(timeoutId);
+      this.timeoutIds = this.timeoutIds.filter((id) => id != timeoutId);
     }, ttlMilliseconds);
     this.timeoutIds.push(timeoutId);
   }
