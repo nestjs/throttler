09291bf834873a9fcb31e697058481043e2cd9a6
Kim 金可明 <koomen@protonmail.com>
2023-01-05 09:28:52 +0100

    feat: rewrite throttler logic to prevent throttle limit bypassing Removes addRecord() and rewrites addRecord() structure from array to object.

--- src/throttler-storage.interface.ts
+++ src/throttler-storage.interface.ts
@@ -2,21 +2,17 @@ export interface ThrottlerStorage {
   /**
    * The internal storage with all the request records.
    * The key is a hashed key based on the current context and IP.
-   * The value of each item wil be an array of epoch times which indicate all
-   * the request's ttls in an ascending order.
+   * The value of is an object contains the two keys:
+   *   - totalHits    (number): Amount of requests being done.
+   *   - timeToExpire (number): Amount of seconds until this expiration.
    */
-  storage: Record<string, number[]>;
-
-  /**
-   * Get a record via its key and return all its request ttls.
-   */
-  getRecord(key: string): Promise<number[]>;
+  storage: Record<string, { totalHits: number; timeToExpire: number }>;
 
   /**
    * Add a record to the storage. The record will automatically be removed from
    * the storage once its TTL has been reached.
    */
-  addRecord(key: string, ttl: number): Promise<void>;
+  addRecord(key: string, ttl: number): Promise<{ totalHits: number; timeToExpire: number }>;
 }
 
 export const ThrottlerStorage = Symbol('ThrottlerStorage');
--- src/throttler.guard.ts
+++ src/throttler.guard.ts
@@ -77,22 +77,20 @@ export class ThrottlerGuard implements CanActivate {
     }
     const tracker = this.getTracker(req);
     const key = this.generateKey(context, tracker);
-    const ttls = await this.storageService.getRecord(key);
-    const nearestExpiryTime = ttls.length > 0 ? Math.ceil((ttls[0] - Date.now()) / 1000) : 0;
+    const { totalHits, timeToExpire } = await this.storageService.addRecord(key, ttl);
 
     // Throw an error when the user reached their limit.
-    if (ttls.length >= limit) {
-      res.header('Retry-After', nearestExpiryTime);
+    if (totalHits > limit) {
+      res.header('Retry-After', timeToExpire);
       this.throwThrottlingException(context);
     }
 
     res.header(`${this.headerPrefix}-Limit`, limit);
     // We're about to add a record so we need to take that into account here.
     // Otherwise the header says we have a request left when there are none.
-    res.header(`${this.headerPrefix}-Remaining`, Math.max(0, limit - (ttls.length + 1)));
-    res.header(`${this.headerPrefix}-Reset`, nearestExpiryTime);
+    res.header(`${this.headerPrefix}-Remaining`, Math.max(0, limit - totalHits));
+    res.header(`${this.headerPrefix}-Reset`, timeToExpire);
 
-    await this.storageService.addRecord(key, ttl);
     return true;
   }
 
