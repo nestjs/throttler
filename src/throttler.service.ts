2e7ab10d17ab5674acbf1565049f7fa3d650c765
Kim 金可明 <koomen@protonmail.com>
2023-01-05 09:28:52 +0100

    feat: rewrite throttler logic to prevent throttle limit bypassing Removes addRecord() and rewrites addRecord() structure from array to object.

--- src/throttler.service.ts
+++ src/throttler.service.ts
@@ -3,31 +3,29 @@ import { ThrottlerStorage } from './throttler-storage.interface';
 
 @Injectable()
 export class ThrottlerStorageService implements ThrottlerStorage, OnApplicationShutdown {
-  private _storage: Record<string, number[]> = {};
+  private _storage: Record<string, { totalHits: number; timeToExpire: number }> = {};
   private timeoutIds: NodeJS.Timeout[] = [];
 
-  get storage(): Record<string, number[]> {
+  get storage(): Record<string, { totalHits: number; timeToExpire: number }> {
     return this._storage;
   }
 
-  async getRecord(key: string): Promise<number[]> {
-    return this.storage[key] || [];
-  }
-
-  async addRecord(key: string, ttl: number): Promise<void> {
+  async addRecord(key: string, ttl: number): Promise<{ totalHits: number; timeToExpire: number }> {
     const ttlMilliseconds = ttl * 1000;
     if (!this.storage[key]) {
-      this.storage[key] = [];
+      this.storage[key] = { totalHits: 0, timeToExpire: ttlMilliseconds };
     }
 
-    this.storage[key].push(Date.now() + ttlMilliseconds);
+    this.storage[key].totalHits++;
 
     const timeoutId = setTimeout(() => {
-      this.storage[key].shift();
+      this.storage[key].totalHits--;
       clearTimeout(timeoutId);
       this.timeoutIds = this.timeoutIds.filter((id) => id != timeoutId);
     }, ttlMilliseconds);
     this.timeoutIds.push(timeoutId);
+
+    return this.storage[key];
   }
 
   onApplicationShutdown() {
