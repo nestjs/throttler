1fbf8a7673dcdfafd4226afc7896b371e22e30e2
Jay McDoniel <jmcdo29@gmail.com>
2023-07-06 11:19:21 -0700

    fix: better write out the time helper utilities

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,5 +1,5 @@
 export const seconds = (howMany: number) => howMany * 1000;
-export const minutes = (howMany: number) => seconds(howMany) * 60;
-export const hours = (howMany: number) => minutes(howMany) * 60;
-export const days = (howMany: number) => hours(howMany) * 24;
-export const weeks = (howMany: number) => days(howMany) * 7;
+export const minutes = (howMany: number) => 60 * howMany * seconds(1);
+export const hours = (howMany: number) => 60 * howMany * minutes(1);
+export const days = (howMany: number) => 24 * howMany * hours(1);
+export const weeks = (howMany: number) => 7 * howMany * days(1);
