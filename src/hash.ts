765af3f6a07bb8a828885ad40a47184ee77512ea
Jay McDoniel <jmcdo29@gmail.com>
2024-08-22 08:20:03 -0700

    fix: use sha256 instead of md5 to support openssl 3.0 and on

--- src/hash.ts
+++ src/hash.ts
@@ -1,5 +1,5 @@
 import * as crypto from 'node:crypto';
 
-export function md5(text: string): string {
-  return crypto.createHash('md5').update(text).digest('hex');
+export function sha256(text: string): string {
+  return crypto.createHash('sha256').update(text).digest('hex');
 }
